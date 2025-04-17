d388c39a341e1c21b37afb31558adfb65ebf80c0
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2025-04-17 16:06:11 +0800

    feat: dynamic change permissions base on request method

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Max
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,6 +15,14 @@ from .serializers import (
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+
+        if self.request.method == "POST":
+            self.permission_classes = (IsAdminUser,)
+
+        return super().get_permissions()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
