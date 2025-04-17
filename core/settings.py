f7003ec4fc9c89adc9731edbddb9c81917cbf645
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2025-04-17 17:45:07 +0800

    feat: add RESTful documentation tool drf-spectacular

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 if DEBUG:
     INSTALLED_APPS += [
         "django_extensions",
+        "drf_spectacular",
         "silk",
     ]
 
@@ -142,4 +143,13 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
 }
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -16,4 +21,19 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         path("silk/", include("silk.urls", namespace="silk")),
+        path(
+            "api/schema/",
+            SpectacularAPIView.as_view(),
+            name="schema",
+        ),
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
     ]
